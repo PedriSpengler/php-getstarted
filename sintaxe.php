3a2db0f875f8b30bdd7df28359af566e5397cf98
pedrispengler <pedroj.oficial@gmail.com>
2025-05-15 14:14:54 -0400

    sintaxe.php

---
+++ sintaxe.php
@@ -0,0 +1,5 @@
+1.  <?php echo 'Se quiser servir código PHP em documentos XHTML ou XML,
+                use essas tags'; ?>
+
+2.  A tag echo curta <?= 'imprima essa string' ?> também pode ser usada.
+    Ela é equivalente a <?php echo 'imprima essa string' ?>.
